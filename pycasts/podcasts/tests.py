2f2f5a34c473e3bffd2d8454fe8a8a344d1c3d5a
emiliopalmerini <emilio.palmerini@gmail.com>
2022-10-22 09:38:25 +0200

    Tests + migration

--- pycasts/podcasts/tests.py
+++ pycasts/podcasts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Episode
+from django.utils import timezone
+
 
 # Create your tests here.
+
+class PodCastsTests(TestCase):
+    def setUp(self):
+        self.episode = Episode.objects.create(
+            title='Test Title',
+            description='Test Description',
+            pub_date=timezone.now(),
+            link='https://www.google.com',
+            image='https://www.google.com',
+            podcast_name='Test Podcast',
+            guid='12345678-1234-1234-1234-123456789012',
+        )
+
+    def test_episode_content(self):
+        self.assertEqual(self.episode.title, 'Test Title')
+        self.assertEqual(self.episode.description, 'Test Description')
+        self.assertEqual(self.episode.podcast_name, 'Test Podcast')
+        self.assertEqual(self.episode.guid, '12345678-1234-1234-1234-123456789012')
+
+    def test_episode_str_representation(self):
+        self.assertEqual(str(self.episode), 'Test Podcast - Test Title')
