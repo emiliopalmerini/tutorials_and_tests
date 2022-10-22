3e7f97d082453721cc85989d6a527818ea9a5042
emiliopalmerini <emilio.palmerini@gmail.com>
2022-10-22 23:49:07 +0200

    Fix

--- pycasts/content_aggregator/settings.py
+++ pycasts/content_aggregator/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qb8d@1mdgmw#g8u419lgm$*lu%$3k=x_rqg1+j1zbkw!xd1yu&'
+SECRET_KEY = 'django-insecure-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
