13b434c04551c97407c583218ca024334a0a9ced
emiliopalmerini <emilio.palmerini@gmail.com>
2022-10-22 09:11:54 +0200

    Created Episode in model + admin access to Episode

--- pycasts/podcasts/admin.py
+++ pycasts/podcasts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Episode
+
 
 # Register your models here.
+
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'podcast_name')
